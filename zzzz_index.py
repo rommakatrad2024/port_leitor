ceacaa326d5afa68867c116182b8cdf8c325315a
rommakatrad2024 <rommakatrad2024@gmail.com>
2024-09-14 14:16:50 +0000

    all

--- zzzz_index.py
+++ zzzz_index.py
@@ -1,10 +1,16 @@
 #pip install gtts
 
+# LINK IDIOMAS https://gtts.readthedocs.io/en/latest/module.html#languages-gtts-lang
+
+# git add --all
+# git commit -a -m "mensage"
+# git push
+
 
 from gtts import gTTS
 
-texto = "voo"
-lingua = "pt"
+texto = "outlook"
+lingua = "en-uk"
 tts = gTTS(texto, lang=lingua)
-tts.save("u_voo.mp3")
+tts.save("u_outlook.mp3")
 
